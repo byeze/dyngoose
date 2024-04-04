70e1cceb0bd5947ed86dcbd23464e52f6544781a
Eze Calonge <eze.calonge@naranjax.com>
2024-04-04 13:52:18 -0300

    remove readonly

--- src/connections/connection.ts
+++ src/connections/connection.ts
@@ -1,5 +1,5 @@
-import { type DynamoDB } from '@aws-sdk/client-dynamodb'
+import { type DynamoDB } from "@aws-sdk/client-dynamodb";
 
 export interface Connection {
-  readonly client: DynamoDB
+  client: DynamoDB;
 }
